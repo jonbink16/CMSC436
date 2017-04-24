3e072cd77e0faf21054d0a74717c4bde75274980
Jon <jonbink16@gmail.com>
2017-04-23 22:54:35 -0400

    added outdoor walking app

---
+++ app/src/main/java/com/example/oscar/cmsc436/activities/OutdoorWalkActivity.java
@@ -0,0 +1,15 @@
+package com.example.oscar.cmsc436.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.oscar.cmsc436.R;
+
+public class OutdoorWalkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outdoor_walk);
+    }
+}
