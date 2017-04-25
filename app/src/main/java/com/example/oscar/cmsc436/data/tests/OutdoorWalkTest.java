afcedbd5d87465456cf62603c295395b57385117
jonbink16 <jonbink16@gmail.com>
2017-04-24 21:52:14 -0400

    added OutdoorWalkingTest data holder

--- app/src/main/java/com/example/oscar/cmsc436/data/tests/OutdoorWalkTest.java
+++ app/src/main/java/com/example/oscar/cmsc436/data/tests/OutdoorWalkTest.java
@@ -5,4 +5,26 @@ package com.example.oscar.cmsc436.data.tests;
  */
 
 public class OutdoorWalkTest {
+
+
+    private long time;
+    private float meters, mps;
+
+    public OutdoorWalkTest(long time, float meters, float mps){
+        this.time = time;
+        this.meters = meters;
+        this.mps = mps;
+    }
+
+    public float getTime(){
+        return (float)time;
+    }
+
+    public float getMps(){
+        return mps;
+    }
+
+    public float getMeters(){
+        return meters;
+    }
 }
